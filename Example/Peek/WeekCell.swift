93b736e84ef914729582b2efb5c0563687f34e3b
Shaps <shapsuk@me.com>
2016-04-23 20:50:14 +0100

    Updated all copyright info

--- Example/Peek/WeekCell.swift
+++ Example/Peek/WeekCell.swift
@@ -1,10 +1,24 @@
-//
-//  WeekCell.swift
-//  Track
-//
-//  Created by Shaps Mohsenin on 09/02/2016.
-//  Copyright © 2016 Snippex. All rights reserved.
-//
+/*
+  Copyright © 23/04/2016 Snippex
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+ */
 
 import UIKit
 import SPXMasking
@@ -53,4 +67,3 @@ class WeekCell: UICollectionViewCell {
   }
   
 }
-
