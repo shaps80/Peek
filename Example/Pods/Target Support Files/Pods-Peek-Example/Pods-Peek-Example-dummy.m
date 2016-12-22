ba9cc8bf5ab5d510f6cfd3d12932c2408609db5e
Shaps Benkau <shapsuk@me.com>
2016-12-22 20:40:31 +0000

    Added missing files to the repo

---
+++ Example/Pods/Target Support Files/Pods-Peek-Example/Pods-Peek-Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Peek_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Peek_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-Peek-Example/Pods-Peek-Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Peek_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Peek_ExampleVersionString[];
+
