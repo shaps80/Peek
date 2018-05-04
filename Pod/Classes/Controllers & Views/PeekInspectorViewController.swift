9495937fe807ef3bb2db161d8a1007a71ac0e0c2
Shaps Benkau <shapsuk@me.com>
2018-05-04 10:33:54 +0100

    Introduced toggle and isNilOrEmpty, subsequently cleaned up some code

--- Pod/Classes/Controllers & Views/PeekInspectorViewController.swift
+++ Pod/Classes/Controllers & Views/PeekInspectorViewController.swift
@@ -270,7 +270,7 @@ internal final class PeekInspectorViewController: PeekSectionedViewController, U
     
     private var isSearching: Bool {
         return searchController.isActive
-            && searchController.searchBar.text?.isEmpty == false
+            && !searchController.searchBar.text.isNilOrEmpty
     }
     
     func updateSearchResults(for searchController: UISearchController) {
@@ -646,13 +646,12 @@ extension PeekInspectorViewController {
     }
     
     func tableView(_ tableView: UITableView, shouldShowMenuForRowAt indexPath: IndexPath) -> Bool {
-        guard let cell = tableView.cellForRow(at: indexPath), let text = cell.detailTextLabel?.text?.trimmingCharacters(in: .whitespacesAndNewlines) else { return false }
-        return !text.isEmpty
+        let text = tableView.cellForRow(at: indexPath)?.detailTextLabel?.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        return text.isNilOrEmpty
     }
     
     func tableView(_ tableView: UITableView, performAction action: Selector, forRowAt indexPath: IndexPath, withSender sender: Any?) {
-        guard let cell = tableView.cellForRow(at: indexPath), let text = cell.detailTextLabel?.text?.trimmingCharacters(in: .whitespacesAndNewlines) else { return }
-        UIPasteboard.general.string = text
+        UIPasteboard.general.string = tableView.cellForRow(at: indexPath)?.detailTextLabel?.text?.trimmingCharacters(in: .whitespacesAndNewlines)
     }
     
 }
